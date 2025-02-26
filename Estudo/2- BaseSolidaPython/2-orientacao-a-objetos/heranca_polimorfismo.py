10d2a65cbf908f62d257f86d584649c8a6ddaa6f
Daniel <marcosdaniel.indiedev@gmail.com>
2025-02-26 14:42:17 -0300

    !

--- Estudo/2- BaseSolidaPython/2-orientacao-a-objetos/heranca_polimorfismo.py
+++ Estudo/2- BaseSolidaPython/2-orientacao-a-objetos/heranca_polimorfismo.py
@@ -1,1 +1,18 @@
-# HERENÇA E POLIMORFISMO
+# HERENÇA, POLIMORFISMO, ENCAPSULAMENTO E ABSTRAÇÃO
+
+# HERANÇA
+# É um mecanismo que permite que uma classe herde atributos e métodos de outra classe.
+# A classe que herda é chamada de subclasse e a classe que é herdada é chamada de superclasse.
+
+# POLIMORFISMO
+# É a capacidade de um objeto poder ser referenciado de várias formas.
+# O polimorfismo permite que um objeto de uma subclasse seja tratado como um objeto de uma superclasse.
+# O polimorfismo é uma característica da orientação a objetos que permite que um objeto possa ser referenciado de várias formas.
+
+# ENCAPSULAMENTO
+# É o mecanismo que restringe o acesso aos atributos e métodos de uma classe.
+# O encapsulamento é uma forma de proteger os atributos e métodos de uma classe, evitando que sejam acessados diretamente.
+
+# ABSTRAÇÃO
+# É a capacidade de abstrair um objeto do mundo real.
+# A abstração é uma forma de simplificar um objeto do mundo real, tornando-o mais fácil de ser representado em um programa.
